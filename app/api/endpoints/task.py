1741e07df92c3a8be7aba38ded1e9f32c7f7125a
pycorn0729 <hayesdominique0729@gmail.com>
2025-03-08 17:55:12 -0600

    feat: implement auth

--- app/api/endpoints/task.py
+++ app/api/endpoints/task.py
@@ -1,10 +1,41 @@
+import binascii
 from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from substrateinterface import Keypair
+
 from app.services.task import (
     get_seed, 
     get_task,
 )
 
+
 router = APIRouter()
+
+
+def verify_signature(request: Request):
+    hotkey = request.headers.get("Hotkey")
+    signature_hex = request.headers.get("Signature")    
+    if not hotkey or not signature_hex:
+        return False
+
+    try:  
+        signature = binascii.unhexlify(signature_hex)
+        data_to_verify = b"I am the owner of the wallet"
+        keypair = Keypair(ss58_address=hotkey)
+        is_valid = keypair.verify(data_to_verify, signature)
+        if not is_valid:
+            return False
+        allowed_hotkeys = [
+            "5FFApaS75bv5pJHfAp2FVLBj9ZaXuFDjEypsaBNc1wCfe52v",
+            "5F4tQyWrhfGVcNhoqeiNsR6KjD4wMZ2kfhLj4oHYuyHbZAc3",
+            "5HEo565WAy4Dbq3Sv271SAi7syBSofyfhhwRNjFNSM2gP9M2",
+            "5F2CsUDVbRbVMXTh9fAzF9GacjVX7UapvRxidrxe7z8BYckQ",
+        ]
+        return hotkey in allowed_hotkeys
+    except Exception as e:
+        return False
 
 
 @router.get("/seed")
@@ -17,7 +48,10 @@ async def seed_task(session:int, task_number:int):
 
 
 @router.get("/generate")
-async def generate_task(session:int, task_number:int):
+async def generate_task(session:int, task_number:int, request: Request):
+    if not verify_signature(request):
+        return ""
+    
     html = get_task(session, task_number)
     return {
         "html": html,
